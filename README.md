74afd2723a034f7f802a071444efff8ce8ed0950
JoKlim <klimesch.jonathan@gmail.com>
2019-10-29 14:35:31 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# MorphX
+Toolkit for exploration and segmentation of 3D morphologies.
+
+Repository is under construction.
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='MorphX',
+    version='0.0.0',
+    description='Toolkit for exploration and segmentation of 3D morphologies in form of point clouds or meshes.',
+    url='https://github.com/StructuralNeurobiologyLab/MorphX',
+    download_url='https://github.com/StructuralNeurobiologyLab/MorphX.git',
+    author='Jonathan Klimesch, Yang Liu, Philipp Schubert',
+    author_email='jklimesch@neuro.mpg.de',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: GPL-2.0 License',
+        'Intended Audience :: Science/Research'
+        'Topic :: Scientific/Engineering :: Morphology Analysis',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
