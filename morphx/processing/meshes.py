bc9914f808e947cccf7d58df3a5c247e21cf9d51
JoKlim <klimesch.jonathan@gmail.com>
2020-02-29 16:11:24 +0100

    Updated poisson disk sampling to open3d method.

--- morphx/processing/meshes.py
+++ morphx/processing/meshes.py
@@ -6,8 +6,7 @@
 # Authors: Jonathan Klimesch
 
 import numpy as np
-import point_cloud_utils as pcu
-from morphx.processing import clouds
+import open3d as o3d
 from morphx.classes.pointcloud import PointCloud
 from morphx.classes.meshcloud import MeshCloud
 from scipy.spatial import cKDTree
@@ -16,8 +15,7 @@ from scipy.spatial import cKDTree
 # -------------------------------------- MESH SAMPLING ------------------------------------------- #
 
 def sample_mesh_poisson_disk(mc: MeshCloud, sample_num: int) -> PointCloud:
-    """ Uses Point-Disk-Sampling as described at https://github.com/fwilliams/point-cloud-utils and maps existing
-        labels using a KDTree.
+    """ Uses poisson disk sampling and maps existing labels using a KDTree.
 
     Args:
         mc: The MeshCloud from which the samples should be generated
@@ -26,20 +24,18 @@ def sample_mesh_poisson_disk(mc: MeshCloud, sample_num: int) -> PointCloud:
     Returns:
         PointCloud consisting of sampled points.
     """
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(mc.vertices)
+    mesh.triangles = o3d.utility.Vector3iVector(mc.faces)
+    sample = mesh.sample_points_poisson_disk(sample_num)
+    s_vertices = np.asarray(sample.points)
 
-    vertices = mc.vertices.astype(float)
-    s_vertices, s_normals = pcu.sample_mesh_poisson_disk(vertices, mc.faces, np.array([]), sample_num)
-
-    # TODO: This can be improved - how?
-    labels = None
     # map labels from input cloud to sample
+    labels = None
     if mc.labels is not None:
         tree = cKDTree(mc.vertices)
         dist, ind = tree.query(s_vertices, k=1)
         labels = mc.labels[ind]
 
-    # sample again, as pcu.sample doesn't always return the requested number of samples
-    # (only a few points must be added)
-    spc, ixs = clouds.sample_objectwise(PointCloud(s_vertices, labels=labels), sample_num)
-
-    return spc
+    result = PointCloud(vertices=s_vertices, labels=labels)
+    return result
