6fdc84bb78964ee64a9c3db664fe1aaab5c94758
Philipp Schubert <phil.jo.schubert@gmail.com>
2021-11-21 14:13:27 +0100

    update to version 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MorphX',
-    version='0.0.1',
+    version='0.1',
     description='Toolkit for exploration and segmentation of 3D morphologies '
                 'in form of point clouds or meshes.',
     url='https://github.com/StructuralNeurobiologyLab/MorphX',
